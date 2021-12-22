e38097b952e992aa6144f7db1c84f0f997dd0979
Natalie Jones <nnjones99@gmail.com>
2021-12-22 09:29:51 -0600

    added more interactivity and completed exercise

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // part 1
-        System.out.println("Howdy, DevmMountain!");
 
-        // part 2
         Scanner input = new Scanner(System.in); // get input
 
-        System.out.println("What is your favorite color?");
-        String color = input.nextLine();  // Get user input
-        System.out.println("Nice, I like " + color + " too");
-
-        // part 3 Mars Adventure Game
         System.out.println("Welcome to the Mars Adventure");
 
         System.out.println("What is your name?");
         String name = input.nextLine();
         System.out.printf("%nHi %s happy to have you aboard", name);
 
-        System.out.println("What color will your spacesuit be?");
-        String suitColor = input.nextLine();
-        System.out.printf("A %s spacesuit, okay sounds good.",suitColor);
+        System.out.println("Are you excited for your grand adventure (y/n)");
+        char isExcited = input.nextLine().charAt(0);
+        if (Character.toLowerCase(isExcited) == 'y') {
+            System.out.println("Yay we're excited to have you here");
+        } else {
+            System.out.println("Wrong answer buddy...you ARE excited.");
+        }
+
+        System.out.println("How many suitcases are you bringing");
+        int numOfSuitcases = Integer.parseInt(input.nextLine());
+        if (numOfSuitcases > 2) {
+            System.out.println("That's too many suitcases. Two is the max.");
+        } else if (numOfSuitcases == 2) {
+            System.out.println("Perfect");
+        } else {
+            System.out.println("Did you bring anything... good packing skills I guess.");
+        }
 
         System.out.println("Do you have pet? (y/n)");
+        String petName;
         String hasPet = input.nextLine();
         if (hasPet.equalsIgnoreCase("y")) {
             System.out.println("Great what is your pet's name?");
-            String petName = input.nextLine();
-            System.out.printf("%nCute name! %s will need a space suit as well. Pick a color", petName);
-            String petColor = input.nextLine();
-            System.out.printf("%nA %s color suit for you, and a %s color suit for %s. You guys look amazing", suitColor, petColor, petName);
+            petName = input.nextLine();
+            System.out.printf("%n %s, cute name! Happy to have you two aboard.", petName);
         } else {
-            System.out.println("Nothing is wrong with being a lone traveler. Good luck and godspeed.");
+            System.out.println("Nothing is wrong with being a lone traveler.");
+            petName = "no pet";
         }
-
+        System.out.printf("Okay %s it's time to go you have %d suitcases and %s is traveling with you.%n", name,numOfSuitcases,petName);
+        for (int i = 0; i < 5; i++){
+            System.out.println(i + "...");
+        }
+        System.out.println("Good luck and godspeed! Blast off!");
     }
 }
